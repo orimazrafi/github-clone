42f2a3e14738901183369fcc89900c416730d34e
ori mazrafi <orimazrafi@gmail.com>
2020-08-09 17:46:04 +0300

    Handle navbar.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './App.scss';
-// import { Container, Box, Button, Grid, Paper } from '@material-ui/core';
 import { Navbar } from './components/Navbar/Navbar';
 import { BrowserRouter as Router } from "react-router-dom"
 function App() {
