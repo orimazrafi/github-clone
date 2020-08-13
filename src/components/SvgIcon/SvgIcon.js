915725d3bc69b6cf6bd5972ea1e6dbee94a29c46
ori mazrafi <orimazrafi@gmail.com>
2020-08-13 12:35:43 +0300

    Handle fix iconbug.

--- src/components/SvgIcon/SvgIcon.js
+++ src/components/SvgIcon/SvgIcon.js
@@ -4,7 +4,7 @@ export const SvgIcon = (props) => {
     return (
         <svg onClick={props.handleClick}
             className={props.hover ? style.hover : style.cursor}
-            style={{ height: props.height, fill: props.fill, position: props.position, right: props.right, top: props.top }} viewBox="0 0 16 16">
+            style={{ margin: props.margin, height: props.height, fill: props.fill, position: props.position, right: props.right, top: props.top }} viewBox="0 0 16 16">
             <path fillRule={props.fillRule} d={props.pathname}
             ></path>
             {props.svgPath2 ?
